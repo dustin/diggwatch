fc7e1820a9f0dd9f017d12e8012fc33495f65e7e
Dustin Sallings <dustin@spy.net>
2007-05-17 00:43:52 -0700

    Support conditional get for comment RSS.
    	It's a bit specific, since it's only for the comment feed, but it's an idea
    of how to do it and it'll at least hopefully cut out a lot of my traffic.

--- src/java/net/spy/diggwatch/UserCommentRSSServlet.java
+++ src/java/net/spy/diggwatch/UserCommentRSSServlet.java
@@ -1,6 +1,9 @@
 package net.spy.diggwatch;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,10 +19,33 @@ public class UserCommentRSSServlet extends BaseDiggServlet {
 	@Override
 	protected void processPath(String path,
 		HttpServletRequest req, HttpServletResponse res) throws Exception {
+		@SuppressWarnings("unchecked") // generic enum
+		Enumeration<String> etagsEnum = req.getHeaders("If-None-Match");
+		Collection<String> eTags=new HashSet<String>();
+		// I generally hate reinventing for loops, but I needed the @suppress
+		while(etagsEnum.hasMoreElements()) {
+			eTags.add(etagsEnum.nextElement());
+		}
 		List<Comment> comments = new ArrayList<Comment>(
 			di.getRelevantComments(path));
-		sendXml(new CommentFeed(path, path
-			+ "'s comments (and replies) on digg", comments), res);
+
+		String eTag=computeEtag(comments);
+		if(eTags.contains(eTag)) {
+			res.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+		} else {
+			res.setHeader("ETag", eTag);
+			sendXml(new CommentFeed(path, path
+					+ "'s comments (and replies) on digg", comments), res);
+		}
+	}
+
+	private String computeEtag(List<Comment> comments) {
+		String rv="0";
+		if(!comments.isEmpty()) {
+			Comment c=comments.get(0);
+			rv=(c.getTimestamp() / 1000) + "-" + c.getEventId();
+		}
+		return rv;
 	}
 
 }
